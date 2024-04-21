d2f01763f7649487d172c4a56c63104658999edf
ExLyD1 <sashaahnij@gmail.com>
2024-04-21 19:36:18 +0300

    almost full landing and adaptive

---
+++ script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 120,
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev'
+  },
+  
+  breakpoints: {
+    1440: {
+      slidesPerView:3,
+    },
+    940: {
+      slidesPerView:2,
+    },
+    320: {
+      slidesPerView:1,
+      centeredSlides: true,
+    }
+  },
+})
